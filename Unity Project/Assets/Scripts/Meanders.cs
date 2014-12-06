e94b4c98d56810224d8f1c260fe768493dcde7f7
Kyle Dobitz <kyledobitz@gmail.com>
2014-12-06 12:16:32 -0800

    fix breaks

--- Unity Project/Assets/Scripts/Meanders.cs
+++ Unity Project/Assets/Scripts/Meanders.cs
@@ -5,15 +5,58 @@ public class Meanders : MonoBehaviour {
 
     public GameObject[] doorways;
     public BoxCollider currentRoom;
-    public float speed;
+    public float minSpeed = 1f;
+    public float maxSpeed = 2f;
+    public float standVsMeanderPercent = 0.5f;
+    public float minDelayTime = 2f;
+    public float maxDelayTime = 5f;
+
+    State currentState = State.STANDING;
+    float nextDecisionTime;
+    Vector3 meanderDestination;
 
 	// Use this for initialization
 	void Start () {
-	
+		nextDecisionTime = Time.time + Random.Range (minDelayTime, maxDelayTime);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
+
+    void FixedUpdate (){
+        switch (currentState)
+        {
+            case State.STANDING:
+                if(Time.time > nextDecisionTime)
+                    currentState = DecideNextState();
+                break;
+            case State.MEANDERING:
+                if(Time.time > nextDecisionTime)
+                    currentState = DecideNextState();
+                break;
+            case State.SWITCHING_ROOMS:
+                break;
+        }
+    }
+   
+
+    State DecideNextState(){
+        if (Random.value < standVsMeanderPercent)
+            return State.STANDING;
+        else
+            return State.MEANDERING;
+		nextDecisionTime = Time.time + Random.Range (minDelayTime, maxDelayTime);
+    }
+
+    void Meander(){
+
+    }
+
+    public enum State{
+        MEANDERING,
+        STANDING,
+        SWITCHING_ROOMS
+    }
 }
