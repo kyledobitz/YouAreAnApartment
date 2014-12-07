8d46b76324265eb0932441ccf3387a847899b855
Cosmo Ray <cosmor@gmail.com>
2014-12-06 17:55:45 -0800

    Transparency Script Working

---
+++ Unity Project/Assets/Scripts/TransparencyAdjustment.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransparencyAdjustment : MonoBehaviour {
+
+    [Range(0,1)]
+    public float alphaAdjust = 1.0f;
+
+    void Start () {
+        
+    }
+
+    void Update() {
+        // guy in a forum said something like this to get children of children
+        // gameObject.GetComponentsInChildren(Transform);
+        foreach (Transform child in gameObject.transform) {
+
+        Color color = child.renderer.material.color;
+        color.a = alphaAdjust;
+        child.renderer.material.color = color;
+        }
+}
+}
