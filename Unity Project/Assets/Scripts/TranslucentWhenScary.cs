ce12e5fc30485fc92b93af9e61ee39aa67285666
nathanhinchey <devnph@gmail.com>
2014-12-06 18:06:12 -0800

    modified some scripts

---
+++ Unity Project/Assets/Scripts/TranslucentWhenScary.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TranslucentWhenScary : MonoBehaviour {
+    public float fearThreshholdToBecomeClear = 0f;
+    public Shader opaque;
+    public Shader transparent;
+	// Use this for initialization
+	void Start () {
+        opaque = Shader.Find("VertexLit");
+        transparent = Shader.Find("Transparent/VertexLit");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ScaredyCat.totalFear >= fearThreshholdToBecomeClear)
+            renderer.material.shader = transparent;
+        else
+            renderer.material.shader = opaque;
+	}
+}
